6ad991b592106ad5e2c991442c1d16413c16a6a3
harish6403 <52630918+harish6403@users.noreply.github.com>
2019-07-07 21:34:29 +0530

    Add files via upload
    new changes

---
+++ Annotation.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class Annotation {
+
+	@Test // this is will execute the test case 
+	
+	public void userlogin() {
+		System.out.println("TestNg was installed successfull");
+	}
+}
